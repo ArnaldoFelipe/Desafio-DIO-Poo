8ba0292c750d19f93ac740b524cd472ce215fbb2
arnaldo felipe <feelipesilvaa11@gmail.com>
2025-08-02 17:54:15 -0300

    criando a classe pai Conteudo e fazendo a herança para suas classes filhas - Curos/Mentoria

--- Dominio/Mentoria.java
+++ Dominio/Mentoria.java
@@ -3,39 +3,32 @@ package DesafioDIO.Dominio;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class Mentoria {
-    
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo {
+
     private LocalDate data;
 
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
     }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+
     public LocalDate getData() {
         return data;
     }
     public void setData(LocalDate data) {
         this.data = data;
     }
+
     @Override
     public String toString() {
         DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         String dataFormatada = this.data.format(formatador);
 
-        return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + dataFormatada + "]";
+        return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + dataFormatada + "]";
     }
 
     
+
     
 }
